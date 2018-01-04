abce3e603218a0477e2391e5f948690cc15f76ab
AceAttorneyMaster111 <NoahS2003@gmail.com>
2018-01-04 12:32:44 -0500

    Added class Client

--- discord-extras.py
+++ discord-extras.py
@@ -2,6 +2,9 @@
 
 Functions: get_user_identifier: Get the unique identifier of a user, in the form User#1234.
            ask_for_message (coroutine): Ask a question to the author of a message, wait for a response, and check it against criteria.
+           reply (coroutine): Send a reply to the channel of a message.
+
+Classes: Client: A subclass of discord.Client that incorporates additional methods.
 
 See the specific function documentation for more details."""
 
@@ -78,3 +81,66 @@ More detail: The client sends a message to message.channel containing prompt, th
 			errtype = "int"
 			# timeout
 		raise TypeError(("Expected '{}'. Received " + re.match(r"'[a-zA-Z]*'", str(a))[0]).format(errtype))
+
+class Client(discord.Client):
+	"""Client: A subclass of discord.Client that incorporates additional methods.
+
+Params: none
+
+Methods: ask_for_message: Ask a question to the author of a message, wait for a response, and check it against criteria.
+	     reply: A shortcut for client.send_message"""
+	async def ask_for_message(self, message, prompt, timeout=30, timeout_msg="Operation timed out.", condition=lambda m: True, fail_msg=""):
+		"""ask_for_message(message, prompt, timeout=30, timeout_msg="Operation timed out.". condition=lambda m: True, fail_msg=""):
+
+An alias for the module-level ask_for_message. Shrinks needed code for getting input from user.
+
+This method is a coroutine. (must be used with await)
+
+Params: message (discord.Message): The original message being responded to
+        prompt (str): The question to ask the user
+Keyword params:
+        timeout (int): The amount of time in seconds to give the user to answer.
+        	Defaults to 30.
+        timeout_msg (str): The message to display to the user if they don't answer in the required amount of time.
+        	Defaults to "Operation timed out."
+        condition (lambda(str): bool): The condition to pass the user's answer through.
+        	Defaults to letting all answers pass. (lambda m: True)
+        fail_msg (str): The message to display to the user if their response returns False when passed through condition.
+        	Defaults to an empty string. ("")
+
+Returns: str containing message content, otherwise None if condition failed or timeout_msg was sent.
+
+More detail: The client sends a message to message.channel containing prompt, then waits for a response from message.author for timeout seconds.
+	         If the author sends nothing in response, the client sends another message containing timeout_msg.
+	         Otherwiser, the message's content will be passed through condition. If it returns False, the client will send fail_msg."""
+	    try:
+			assert isinstance(timeout, int)
+			await reply(self, message.channel, prompt)
+			answer = await self.wait_for_message(channel=message.channel, author=message.author, timeout=timeout)
+			if answer is None:
+				await reply(self, message.channel, timeout_msg)
+				return None
+			if not condition(answer.content):
+				await reply(self, message.channel, fail_msg)
+				return None
+			return message.content
+		except Exception as e:
+			if isinstance(e, AttributeError):
+				errtype = "discord.Message"
+				# message
+			elif isinstance(e, TypeError):
+				errtype = "lambda"
+				# condition
+			elif isinstance(e, AssertionError):
+				errtype = "int"
+				# timeout
+			raise TypeError(("Expected '{}'. Received " + re.match(r"'[a-zA-Z]*'", str(a))[0]).format(errtype))
+	async def reply(self, message, text):
+		"""reply(message, text): An alias for the module-level reply. Shrinks needed code for sending a reply to a message's channel.
+
+This method is a coroutine. (must be used with await).
+
+Params: client (discord.Client): The client from which to send message.
+        message (discord.Message): The message being replied to.
+        text (str): The text for the reply."""
+        await self.send_message(message.channel, text)
